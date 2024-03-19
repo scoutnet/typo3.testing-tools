fe435dec55491d58742ef27e9bf7c12509f96783
Stefan Horst <muetze@scoutnet.de>
2024-03-19 21:24:56 +0100

    new patch 3.0.14

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,7 +17,7 @@ $EM_CONF[$_EXTKEY] = [
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '3.0.13',
+	'version' => '3.0.14',
 	'constraints' => [
 		'depends' => [
 		],
