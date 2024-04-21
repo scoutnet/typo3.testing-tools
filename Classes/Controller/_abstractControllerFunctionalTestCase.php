11e755063c5ff1185bb698982ff775989dbbacfe
Stefan Horst <muetze@scoutnet.de>
2024-04-21 13:14:50 +0200

    cglFix

--- Classes/Controller/_abstractControllerFunctionalTestCase.php
+++ Classes/Controller/_abstractControllerFunctionalTestCase.php
@@ -16,8 +16,6 @@ namespace ScoutNet\TestingTools\Controller;
 
 use GuzzleHttp\Psr7\Uri;
 
-use TYPO3\CMS\Core\Database\Schema\SchemaMigrator;
-use TYPO3\CMS\Core\Database\Schema\SqlReader;
 use function ini_get;
 
 use JsonException;
