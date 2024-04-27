ff907e45585198b76a1b987f03a38e7a467d347e
Stefan Horst <muetze@scoutnet.de>
2024-04-27 12:22:07 +0200

    remove createServerRequest from ViewHelperFunctionalTestCase since it is now in FunctionalTestCase

--- Classes/ViewHelpers/_abstractViewHelperFunctionalTestCase.php
+++ Classes/ViewHelpers/_abstractViewHelperFunctionalTestCase.php
@@ -14,14 +14,8 @@
 
 namespace ScoutNet\TestingTools\ViewHelpers;
 
-use Psr\Http\Message\ServerRequestInterface;
-use ScoutNet\ScoutnetStructure\ViewHelpers\Structure\SelectViewHelper;
 use ScoutNet\TestingTools\_abstractFunctionalTestCase;
-use TYPO3\CMS\Core\Core\SystemEnvironmentBuilder;
-use TYPO3\CMS\Core\Http\NormalizedParams;
-use TYPO3\CMS\Core\Http\ServerRequest;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Mvc\ExtbaseRequestParameters;
 use TYPO3\CMS\Extbase\Mvc\Request;
 use TYPO3\CMS\Fluid\View\StandaloneView;
 
@@ -120,54 +114,5 @@ abstract class _abstractViewHelperFunctionalTestCase extends _abstractFunctional
 
         $view->setTemplateSource('<' . $this->viewHelper . ' ' . $variables . ' >' . $this->subViewTemplate . '</' . $this->viewHelper . '>');
         self::assertSame($expected, $view->render());
-    }
-
-    protected function createServerRequest(string $url, string $method = 'GET'): ServerRequestInterface
-    {
-        $requestUrlParts = parse_url($url);
-        $docRoot = $this->instancePath;
-
-        // @todo: Remove when dropping support for v12
-        $hasConsolidatedHttpEntryPoint = class_exists(CoreHttpApplication::class);
-        $scriptPrefix = $hasConsolidatedHttpEntryPoint ? '' : '/typo3';
-
-        $serverParams = [
-            'DOCUMENT_ROOT' => $docRoot,
-            'HTTP_USER_AGENT' => 'TYPO3 Functional Test Request',
-            'HTTP_HOST' => $requestUrlParts['host'] ?? 'localhost',
-            'SERVER_NAME' => $requestUrlParts['host'] ?? 'localhost',
-            'SERVER_ADDR' => '127.0.0.1',
-            'REMOTE_ADDR' => '127.0.0.1',
-            'SCRIPT_NAME' => $scriptPrefix . '/index.php',
-            'PHP_SELF' => $scriptPrefix . '/index.php',
-            'SCRIPT_FILENAME' => $docRoot . '/index.php',
-            'PATH_TRANSLATED' => $docRoot . '/index.php',
-            'QUERY_STRING' => $requestUrlParts['query'] ?? '',
-            'REQUEST_URI' => $requestUrlParts['path'] . (isset($requestUrlParts['query']) ? '?' . $requestUrlParts['query'] : ''),
-            'REQUEST_METHOD' => $method,
-        ];
-        // Define HTTPS and server port
-        if (isset($requestUrlParts['scheme'])) {
-            if ($requestUrlParts['scheme'] === 'https') {
-                $serverParams['HTTPS'] = 'on';
-                $serverParams['SERVER_PORT'] = '443';
-            } else {
-                $serverParams['SERVER_PORT'] = '80';
-            }
-        }
-
-        // Define a port if used in the URL
-        if (isset($requestUrlParts['port'])) {
-            $serverParams['SERVER_PORT'] = $requestUrlParts['port'];
-        }
-        // set up normalizedParams
-        $request = new ServerRequest($url, $method, null, [], $serverParams);
-
-        $request = $request
-            ->withAttribute('normalizedParams', NormalizedParams::createFromRequest($request))
-            ->withAttribute('extbase', new ExtbaseRequestParameters(SelectViewHelper::class))
-            ->withAttribute('applicationType', SystemEnvironmentBuilder::REQUESTTYPE_FE);
-
-        return $request;
     }
 }
