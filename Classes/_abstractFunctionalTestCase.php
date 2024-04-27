a32aea8d27086b87df2ec02dbe1b5ce60231d7dd
Stefan Horst <muetze@scoutnet.de>
2024-04-27 11:38:21 +0200

    add createServerRequest to functionalTestCase

--- Classes/_abstractFunctionalTestCase.php
+++ Classes/_abstractFunctionalTestCase.php
@@ -15,11 +15,17 @@
 namespace ScoutNet\TestingTools;
 
 use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use ScoutNet\ScoutnetStructure\ViewHelpers\Structure\SelectViewHelper;
+use TYPO3\CMS\Core\Core\SystemEnvironmentBuilder;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\Schema\SchemaMigrator;
 use TYPO3\CMS\Core\Database\Schema\SqlReader;
+use TYPO3\CMS\Core\Http\NormalizedParams;
+use TYPO3\CMS\Core\Http\ServerRequest;
 use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Mvc\ExtbaseRequestParameters;
 use TYPO3\TestingFramework\Core\BaseTestCase;
 use TYPO3\TestingFramework\Core\Exception;
 use TYPO3\TestingFramework\Core\Functional\Framework\DataHandling\Snapshot\DatabaseAccessor;
@@ -298,5 +304,54 @@ abstract class _abstractFunctionalTestCase extends FunctionalTestCase
 
         $insertStatements = $sqlReader->getInsertStatementArray($sqlCode);
         $schemaMigrationService->importStaticData($insertStatements);
+    }
+
+    protected function createServerRequest(string $url, string $method = 'GET'): ServerRequestInterface
+    {
+        $requestUrlParts = parse_url($url);
+        $docRoot = $this->instancePath;
+
+        // @todo: Remove when dropping support for v12
+        $hasConsolidatedHttpEntryPoint = class_exists(CoreHttpApplication::class);
+        $scriptPrefix = $hasConsolidatedHttpEntryPoint ? '' : '/typo3';
+
+        $serverParams = [
+            'DOCUMENT_ROOT' => $docRoot,
+            'HTTP_USER_AGENT' => 'TYPO3 Functional Test Request',
+            'HTTP_HOST' => $requestUrlParts['host'] ?? 'localhost',
+            'SERVER_NAME' => $requestUrlParts['host'] ?? 'localhost',
+            'SERVER_ADDR' => '127.0.0.1',
+            'REMOTE_ADDR' => '127.0.0.1',
+            'SCRIPT_NAME' => $scriptPrefix . '/index.php',
+            'PHP_SELF' => $scriptPrefix . '/index.php',
+            'SCRIPT_FILENAME' => $docRoot . '/index.php',
+            'PATH_TRANSLATED' => $docRoot . '/index.php',
+            'QUERY_STRING' => $requestUrlParts['query'] ?? '',
+            'REQUEST_URI' => $requestUrlParts['path'] . (isset($requestUrlParts['query']) ? '?' . $requestUrlParts['query'] : ''),
+            'REQUEST_METHOD' => $method,
+        ];
+        // Define HTTPS and server port
+        if (isset($requestUrlParts['scheme'])) {
+            if ($requestUrlParts['scheme'] === 'https') {
+                $serverParams['HTTPS'] = 'on';
+                $serverParams['SERVER_PORT'] = '443';
+            } else {
+                $serverParams['SERVER_PORT'] = '80';
+            }
+        }
+
+        // Define a port if used in the URL
+        if (isset($requestUrlParts['port'])) {
+            $serverParams['SERVER_PORT'] = $requestUrlParts['port'];
+        }
+        // set up normalizedParams
+        $request = new ServerRequest($url, $method, null, [], $serverParams);
+
+        $request = $request
+            ->withAttribute('normalizedParams', NormalizedParams::createFromRequest($request))
+            ->withAttribute('extbase', new ExtbaseRequestParameters(SelectViewHelper::class))
+            ->withAttribute('applicationType', SystemEnvironmentBuilder::REQUESTTYPE_FE);
+
+        return $request;
     }
 }
